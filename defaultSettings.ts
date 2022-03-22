177bbc347f63a11a7e6504eea2bd596d89157233
吴渊 <wrzmonkey@gmail.com>
2022-03-22 17:41:32 +0800

    delete: ignore sensitive file

--- defaultSettings.ts
+++
@@ -1,7 +0,0 @@
-export const DEV_BASEURL = 'http://bi-test.bbgdata.com';
-export const TEST_BASEURL = '';
-export const RELEASE_BASEURL = '';
-
-export const DATA_DEV_BASEURL = 'http://test-data.bbgdata.com';
-export const DATA_TEST_BASEURL = 'https://test-data.bbgdata.com';
-export const DATA_RELEASE_BASEURL = 'https://data.bbgdata.com';
